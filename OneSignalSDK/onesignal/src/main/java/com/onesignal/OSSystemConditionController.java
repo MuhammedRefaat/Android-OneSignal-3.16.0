4feadb8b5213354d394c4c300c78f0c4b666dcd4
MuhammedRefaat <mrefaat.7889@gmail.com>
2021-12-15 18:04:39 +0400

    Android X updates

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OSSystemConditionController.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSSystemConditionController.java
@@ -2,7 +2,7 @@ package com.onesignal;
 
 import android.content.Context;
 import androidx.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
